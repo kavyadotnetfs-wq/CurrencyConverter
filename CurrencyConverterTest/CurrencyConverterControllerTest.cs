f581090a1d5b9266e9702d897ecd732d4b20817c
kavyadotnetfs-wq <kavyadotnetfs@gmail.com>
2026-06-18 10:34:59 +0530

    changed the path for test project

---
+++ CurrencyConverterTest/CurrencyConverterControllerTest.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using CurrencyConverter.Controllers;
+using CurrencyConverter.Models;
+using CurrencyConverter.Services;
+using Microsoft.Extensions.Logging;
+using Castle.Core.Logging;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CurrencyConverterTest
+{
+    public class CurrencyConverterControllerTest
+    {
+        private readonly Mock<ICurrencyConverterService> _service;
+        private readonly Mock<ILogger<CurrencyConverterController>> _logger;
+
+        public CurrencyConverterControllerTest()
+        {
+            _service = new Mock<ICurrencyConverterService>();
+            _logger  = new Mock<ILogger<CurrencyConverterController>>();
+        }
+        [Fact]
+        public async Task Covert_Success_flow()
+        {
+            var currencyController = new CurrencyConverterController(_service.Object, _logger.Object);
+                       
+            var converted_amt = new CurrencyDetails()
+            {
+                ExchangeRate = 2,
+                ConvertedAmount = 2
+            };
+            var res = _service.Setup(s => s.Convert(It.IsAny<string>(),It.IsAny<string>(),It.IsAny<Decimal>()))
+            .ReturnsAsync(converted_amt);
+            var controllerConvert =await currencyController.Convert("USD", "INR", 2);
+            var result = Assert.IsType<OkObjectResult>(controllerConvert);
+            Assert.Equal(200, result.StatusCode);
+            
+        }
+        [Fact]
+        public async Task Convert_SourceCurrencyIsNullOrWhiteSpace_Return400BadRequestError()
+        {
+            var currencyController = new CurrencyConverterController(_service.Object,_logger.Object);
+            var controller_Result =await currencyController.Convert("", "INR", 2);
+            var result = Assert.IsType<BadRequestResult>(controller_Result);
+            Assert.Equal(400, result.StatusCode);
+        }
+        [Fact]
+        public async Task Convert_TargetCurrencyIsNullOrWhiteSpace_Return400BadRequestError()
+        {
+            var currencyController = new CurrencyConverterController(_service.Object, _logger.Object);
+            var controller_Result = await currencyController.Convert("USD", "", 2);
+            var result = Assert.IsType<BadRequestResult>(controller_Result);
+            Assert.Equal(400, result.StatusCode);
+        }
+        [Fact]
+        public async Task Convert_AmountIsValidOrNot_Return400BadRequestError()
+        {
+            var currencyController = new CurrencyConverterController(_service.Object, _logger.Object);
+            var controller_Result = await currencyController.Convert("USD", "INR", 0);
+            var result = Assert.IsType<BadRequestResult>(controller_Result);
+            Assert.Equal(400, result.StatusCode);
+
+        }
+        [Fact]
+        public async Task Convert_SourceCurrencyCharLengthCheck_Return400BadRequestError()
+        {
+            var currencyController = new CurrencyConverterController(_service.Object, _logger.Object);
+            var controller_Result = await currencyController.Convert("USDDD", "INR", 2);
+            var result = Assert.IsType<BadRequestResult>(controller_Result);
+            Assert.Equal(400,result.StatusCode);
+        }
+        [Fact]
+        public async Task Convert_SourceCurrencyCharCheck_Return400BadRequestError()
+        {
+            var currencyController = new CurrencyConverterController(_service.Object, _logger.Object);
+            var controller_Result = await currencyController.Convert("USD123", "INR", 2);
+            var result = Assert.IsType<BadRequestResult>(controller_Result);
+            Assert.Equal(400, result.StatusCode);
+        }
+        [Fact]
+        public async Task Convert_TargetCurrencyCharLengthCheck_Return400BadRequestError()
+        {
+            var currencyController = new CurrencyConverterController(_service.Object, _logger.Object);
+            var controller_Result = await currencyController.Convert("USD", "INRRRR", 2);
+            var result = Assert.IsType<BadRequestResult>(controller_Result);
+            Assert.Equal(400, result.StatusCode);
+        }
+        [Fact]
+        public async Task Convert_TargetCurrencyCharCheck_Return400BadRequestError()
+        {
+            var currencyController = new CurrencyConverterController(_service.Object, _logger.Object);
+            var controller_Result = await currencyController.Convert("USD", "INR123", 2);
+            var result = Assert.IsType<BadRequestResult>(controller_Result);
+            Assert.Equal(400, result.StatusCode);
+        }
+        [Fact]
+        public async Task Convert_SourceCurrencyIsEqualstoTargetCurrencyCheck_Return400BadRequestError()
+        {
+            var currencyController = new CurrencyConverterController(_service.Object, _logger.Object);
+            var controller_Result = await currencyController.Convert("USD", "USD", 2);
+            var result = Assert.IsType<BadRequestResult>(controller_Result);
+            Assert.Equal(400, result.StatusCode);
+        }
+    }
+}
---
+++ CurrencyConverterTest/CurrencyConverterServiceTest.cs
@@ -0,0 +1,39 @@
+﻿using CurrencyConverter.Controllers;
+using CurrencyConverter.Models;
+using CurrencyConverter.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using Moq;
+
+namespace CurrencyConverterTest
+{
+    public class CurrencyConverterServiceTest
+    {
+        private readonly Mock<ILogger<CurrencyConverterService>> _logger;
+        private readonly Mock<IConfiguration> _config;
+
+        public CurrencyConverterServiceTest()
+        {
+            _logger = new Mock<ILogger<CurrencyConverterService>>();
+            _config = new Mock<IConfiguration>();
+        }
+
+        [Fact]
+        public async Task Convert_Service_SuccessFlow()
+        {
+            var currencyService = new CurrencyConverterService(_config.Object, _logger.Object);
+            _config.Setup(c => c["INR_TO_USD"]).Returns("0.013"); 
+            var result = await currencyService.Convert("INR", "USD", 1);
+            Assert.True(result.ConvertedAmount > 0);
+        }
+        [Fact]
+        public async Task Convert_Service_Failure_Flow()
+        {
+            var currencyService = new CurrencyConverterService(_config.Object, _logger.Object);
+            _config.Setup(c => c["INR_TO_USD"]).Returns("-0.013");
+            var result = await currencyService.Convert("INR", "USD", 1);
+            Assert.True(result.ConvertedAmount <= 0);
+        }
+    }
+}
